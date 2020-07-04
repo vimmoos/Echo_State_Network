5a7152a1d6ecc667bcccf5a06d4c9f2da02c8fa1
sneha-lodha <s.lodha@student.rug.nl>
2020-07-04 17:17:41 +0200

    metrics enum

--- project/stats/metrics.py
+++ project/stats/metrics.py
@@ -1,27 +1,56 @@
 import random
-
+from dataclasses import dataclass
 import numpy as np
+from aenum import extend_enum, Enum
+import project.esn.utils as u
+from functools import partial
+from scipy.spatial.distance import cdist 
 
+class Metrics(Enum):
+    pass
 
-def mse(output, desired):
-    return sum(np.square(desired - output)) / len(output)
+@dataclass(init=True, repr=True, frozen = True)
+class Metric():
+    _metric: callable
+    output: list
+    target: list
 
+    def __call__(self):
+        return self._metric(self.output, self.target)
 
+def add_metric(func):
+    inner = lambda *args, **kwargs: Metric(func, *args, **kwargs)
+    name = func.__name__
+
+    extend_enum(Metrics, name, inner)
 
 
 teacher_log = np.vectorize(lambda out, teach: out
                            if teach >= 1 else 1 - out)
 
+@add_metric
+def mse(output, desired):
+    return sum(np.square(desired - output)) / len(output)
 
+@add_metric
 def teacher_loss_1d(output, teacher):
     return sum(teacher_log(output,teacher)) / len(output)
 
-
+@add_metric
 def teacher_loss_nd(output, teacher):
     return [
         teacher_loss_1d(output[:, dim], teacher[:, dim])
         for dim in range(output.shape[1])
     ]
 
-# if (__name__ == "__main__"):
-#     print(teacher_loss_nd(out, teacher, len(teacher)))
+@add_metric
+def euclidian_distance(output, desired):
+    return cdist(output, desired, 'euclidean')
+
+@add_metric
+def manhattan_distance(output, desired):
+    return cdist(output, desired, 'cityblock')
+
+@add_metric
+def hamming_distance(output, desired):
+    return cdist(output, desired, 'hamming')
